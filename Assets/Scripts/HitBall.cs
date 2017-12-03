e647e7d2be1abff757cda7080b51e5af61794bc5
LAPTOP-4VAAHV42\Dreaming traveler <dreaming34523@gmail.com>
2017-12-03 15:32:24 +0800

    feat(hitter): Add force table.(Hitting force relates to swing timing)

--- Assets/Scripts/HitBall.cs
+++ Assets/Scripts/HitBall.cs
@@ -9,8 +9,11 @@ public class HitBall : MonoBehaviour {
 	GameObject hitting_point;
 	GameObject pitcher;
 	Rigidbody rig;
+
+	public float hitting_force;
 	public bool isSwing = false;
 	public bool canHitBall = false;
+	public Dictionary<string,string> forceTable = new Dictionary<string,string> ();
 	//Vector3 pitchPos = GameObject.Find ("Pitching_Point").transform.position;
 	// Use this for initialization
 	void Start () {
@@ -18,6 +21,7 @@ public class HitBall : MonoBehaviour {
 		//ball = GameObject.FindGameObjectWithTag("Ball");
 		pitcher = GameObject.FindGameObjectWithTag ("Pitcher");
 		hitting_point = GameObject.Find ("Hitting_Point");
+		CreateForceTable ();
 	}
 	// Update is called once per frame
 	void Update () {
@@ -25,7 +29,22 @@ public class HitBall : MonoBehaviour {
 			animator.SetTrigger ("isHit");
 		}
 	}
-		
+
+	private void CreateForceTable(){
+		float ballForce = 5f;
+		for (float ballPos = 346f; ballPos <= 448.1f; ballPos += 0.1f) {
+			forceTable.Add (ballPos.ToString("0.0"),ballForce.ToString("0.0"));
+			if (ballPos < 397f) {
+				ballForce += 0.2f;
+			} else {
+				ballForce -= 0.2f;
+			}
+		}
+		foreach (KeyValuePair<string,string>kvp in forceTable) {
+			print (kvp.Key + " | " + kvp.Value); 
+		}
+	}
+
 	public bool CanHit(GameObject ball){
 		Vector3 ballPos = ball.transform.position;
 		if(ballPos.x >= 171.0f && ballPos.x <= 220.0f && ballPos.y >= 11.0f && ballPos.y <= 27.0f && ballPos.z >= 176.0f && ballPos.z <= 228.0f){
@@ -33,12 +52,20 @@ public class HitBall : MonoBehaviour {
 		}
 		return false;
 	}	
+
+	private void MatchForce(string ballPos){
+		if (forceTable.ContainsKey (ballPos)) {
+			hitting_force = float.Parse(forceTable [ballPos]);
+		}
+	}
 	public void Swing(GameObject ball){
 		Vector3 pitchPos = GameObject.Find ("Pitching_Point").transform.position;
 		Vector3 hitting_point = GameObject.Find ("Hitting_Point").transform.position;
-		print (ball.transform.position);
+		float ballHorPos = ball.transform.position.x + ball.transform.position.z;
+		print (ballHorPos.ToString("0.0"));
+		MatchForce (ballHorPos.ToString("0.0"));
 		if (CanHit (ball)) {
-			ball.GetComponent<Rigidbody> ().velocity = (new Vector3 (hitting_point.x, Input.mousePosition.y, hitting_point.z)).normalized * 88.2f;
+			ball.GetComponent<Rigidbody> ().velocity = (new Vector3 (hitting_point.x, Input.mousePosition.y, hitting_point.z)).normalized * hitting_force;
 		} else {
 			pitcher.GetComponent<Pitch> ().strike++;
 		}
